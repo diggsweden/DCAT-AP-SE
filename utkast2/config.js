6541fb569c657c092561d6e169a289717a80beea
Matthias Palmer <matthias@metasolutions.se>
2019-11-18 17:25:18 +0100

    Fixar för versionshistorik

---
+++ utkast2/config.js
@@ -0,0 +1,22 @@
+rdforms.spec({
+  language: document.targetLanguage,
+  namespaces: {
+    spdx: 'http://spdx.org/rdf/terms#',
+    adms: 'http://www.w3.org/ns/adms#',
+    prov: 'http://www.w3.org/ns/prov#'
+  },
+  bundles: [
+    ['../bundle.json']
+  ],
+  main: [
+    'dcat:Dataset',
+    'dcat:DatasetService',
+    'dcat:Catalog',
+    'dcat:Distribution'
+  ],
+  supportive: [
+    'dcat:contactPoint',
+    'dcat:foaf:Agent',
+    'dcat:Documentish'
+  ]
+});
