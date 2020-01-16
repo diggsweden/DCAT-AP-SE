296c2f8f73685679a667aab1c2df7c9195b92475
Matthias Palmer <matthias@metasolutions.se>
2020-01-16 15:26:54 +0100

    Utkast 3

--- config.js
+++ config.js
@@ -3,20 +3,32 @@ rdforms.spec({
   namespaces: {
     spdx: 'http://spdx.org/rdf/terms#',
     adms: 'http://www.w3.org/ns/adms#',
-    prov: 'http://www.w3.org/ns/prov#'
+    prov: 'http://www.w3.org/ns/prov#',
+    dcatap: 'http://data.europa.eu/r5r/'
   },
   bundles: [
     ['../bundle.json']
   ],
   main: [
-    'dcat:Dataset',
-    'dcat:DataService',
     'dcat:Catalog',
-    'dcat:Distribution'
+    'dcat:Dataset',
+    'dcat:Distribution',
+    'dcat:DataService',
+    'dcat:foaf:Agent',
+    'dcat:contactPoint',
   ],
   supportive: [
-    'dcat:contactPoint',
-    'dcat:foaf:Agent',
-    'dcat:Documentish'
+    'foaf:Document',
+    'dcterms:LicenseDocument',
+    'dcterms:Standard',
+    'dcat:dcterms:spatial_bb_da',
+    'dcat:dcterms:temporal_da',
+    'dcat:odrs:RSSA',
+    'dcat:prov:qualifiedAttribution',
+    'dcat:qualifiedRelation',
+    'dcat:dcterms:provenance_da',
+    'dcat:spdx:checksum_di',
+    'vc:hasTelephone',
+    'vc:hasAddress'
   ]
 });
