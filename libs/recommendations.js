84f1001bd7a2b268e701013c91879df2b3c247de
Matthias Palmer <matthias@metasolutions.se>
2020-01-23 16:56:51 +0100

    Stöd länkar till rekommendationer

---
+++ libs/recommendations.js
@@ -0,0 +1,63 @@
+const docReady = function(fn) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+    // call on next available tick
+    setTimeout(fn, 1);
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+};
+
+var lang2label = {
+  'sv': 'Rekommendation',
+  'en': 'Recommendation'
+};
+
+docReady(function() {
+  const refs = {};
+  var recs = window.spec2recs;
+  for (var key in recs) if (recs.hasOwnProperty(key)) {
+    var ids = recs[key];
+    for (var i = 0; i < ids.length; i++) {
+      var id = ids[i];
+      refs[id] = refs[id] || [];
+      refs[id].push(key);
+    }
+  }
+
+  for (var anchor in refs) if (refs.hasOwnProperty(anchor)) {
+    var targets = refs[anchor];
+    try {
+      var a = document.getElementById(anchor);
+      var tbody = a.nextElementSibling.firstElementChild;
+      var tr = document.createElement('tr');
+      var th = document.createElement('th');
+      th.innerText = lang2label[document.targetLanguage];
+      tr.appendChild(th);
+      var td = document.createElement('td');
+      tr.appendChild(td);
+      tbody.appendChild(tr);
+
+      for (var j = 0; j < targets.length; j++) {
+        var target = targets[j];
+        var textLinkArr = target.split('-');
+        var textLink = textLinkArr[0] + '. ';
+        textLinkArr.shift();
+        textLink += textLinkArr[0].substr(0, 1).toUpperCase() + textLinkArr[0].substr(1) + ' ';
+        textLinkArr.shift();
+        textLink += textLinkArr.join(' ');
+
+        var recLink = document.createElement('a');
+        recLink.innerText = textLink;
+        recLink.href = 'https://metasolutionsab.github.io/DCAT-AP-SE/docs/recommendations.html#' + target;
+        td.appendChild(recLink);
+        if ((j + 1) < targets.length) {
+          var separator = document.createTextNode(', ');
+          td.appendChild(separator);
+        }
+      }
+    } catch (e) {
+      console.log("Failed create recommendation link from " + id + " to " + key);
+    }
+  }
+});
---
+++ libs/spec2rec.js
@@ -0,0 +1,4 @@
+window.spec2recs = {
+  "1-bra-namn-ska-vara-korta-och-beskrivande": ["dcat_Catalog-dcterms_title", "dcat_Dataset-dcterms_title"],
+  "2-översätt-fritextfält-till-andra-språk": ["dcat_Catalog-dcterms_title", "dcat_Dataset-dcterms_title"],
+};
