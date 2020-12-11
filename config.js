5830274f0975cd085603e7d5b6cf731a4dabc978
Matthias Palmer <matthias@metasolutions.se>
2020-12-11 10:13:11 +0100

    Broken links fix

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var spec2recs = {
     "dcat_Distribution-dcterms_title",
     "dcat_DataService-dcterms_title",
   ],
-  "2-översätt-fritextfält-till-andra-språk": [
+  "2-oversatt-fritextfalt-till-andra-sprak": [
     "dcat_Catalog-dcterms_title",
     "dcat_Dataset-dcterms_title",
     "dcat_Distribution-dcterms_title",
@@ -22,15 +22,15 @@ var spec2recs = {
     "dcat_Distribution-dcterms_format-2",
     "dcat_Distribution-dcterms_format-3"
   ],
-  "4-vissa-datamängder-delas-med-fördel-upp-i-flera-filer": [
+  "4-vissa-datamangder-delas-med-fordel-upp-i-flera-filer": [
     "dcat_Distribution-dcat_downloadURL",
     "dcat_Dataset-dcat_distribution"
   ],
-  "5-att-koppla-samman-en-datamängd-med-en-arkivredovisning": [
+  "5-att-koppla-samman-en-datamangd-med-en-arkivredovisning": [
     "dcat_Dataset-dcterms_identifier",
     "dcat_Dataset-dcat_distribution"
   ],
-  "6-hur-en-datatjänst-används": [
+  "6-hur-en-datatjanst-används": [
     "dcat_Catalog-dcat_service",
     "dcat_Distribution-dcat_accessService"
   ],
@@ -39,11 +39,11 @@ var spec2recs = {
     "dcat_Dataset-dcterms_modified",
     "dcat_Dataset-dcterms_accrualPeriodicity"
   ],
-  "8-tidsperiod-och-tidsupplösning": [
+  "8-tidsperiod-och-tidsupplosning": [
     "dcat_Dataset-dcterms_temporal",
     "dcat_Dataset-dcat_temporalResolution"
   ],
-  "9-utgivare-producent-och-andra-aktörsroller": [
+  "9-utgivare-producent-och-andra-aktorsroller": [
     "dcat_Dataset-dcterms_publisher",
     "dcat_Dataset-dcterms_creator",
     "dcat_Dataset-prov_qualifiedAttribution"
